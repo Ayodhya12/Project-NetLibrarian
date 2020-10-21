b4301fd2ace16bc74188ab257d65fa765a3fb993
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-21 21:30:48 +0530

    Create return.php

---
+++ newProject/return.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="theme.css">
+</head>
+<body>
+	<div class="container cont1">
+    <div class="login-box">
+		<h1 align="center">Book Details</h1>
+        <h2 style="color:#b1adad;">Return a book</h2>
+	    <form method="post" action="">
+            <div class="textbox">
+                <input id="" name="book_id" type="text" placeholder="Enter Book ID Number" required=""> 
+            </div>
+            <div class="textbox">
+                <input id="" type="mem_id" name="title"  placeholder="Enter Member ID Number" required="">
+            </div>
+            <div class="textbox">
+                <input id="" name="ret_date" type="text" placeholder="Enter Current Date" required=""> 
+            </div><br><br>
+            <input type="submit" name="Return" value="Return"  class=" btn"><br><br>
+            <input type="reset" name="Reset"   class=" btn">
+
+    
+        </form>
+    </div>
+  </div>
+</body>
+</html>
