062c67d5996ee3f16cdd91f88f78c24a09f7fa32
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-21 21:31:25 +0530

    Create footer.php

---
+++ newProject/footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     
+	 
+</head>
+<body>
+<!--<footer style="background-color: black; height: 200px">
+	<br>
+	<h3 style="color:white;text-align: center;">Contact us through social media</h3><br>
+
+	<div style="margin:0px 570px;">
+
+		<a href="#" class="fa fa-facebook"></a>
+		<a href="#" class="fa fa-twitter"></a>
+		<a href="#" class="fa fa-google"></a>
+		<a href="#" class="fa fa-instagram"></a>
+		<a href="#" class="fa fa-yahoo"></a>
+	</div>
+
+	<br>
+	<p style="color:white;text-align: center;">
+		<br>
+		Email:&nbsp Online.library@gmail.com <br><br>
+		Mobile:&nbsp &nbsp +880171*******
+	</p>
+</footer>-->
+  <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
+  <a href="#" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Powered by <a href="#" target="_blank" class="w3-hover-text-green"></a></p>
+</footer> 
+</body>
+</html>
+   
