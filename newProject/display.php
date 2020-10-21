62455ff7ccc2682cba9a7ead246c6d7a555df494
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-21 21:31:47 +0530

    Update display.php

--- newProject/display.php
+++ newProject/display.php
@@ -1,4 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="theme.css">
+</head>
+<body>
+<h1 align="center" style="color: #fff; border-bottom: 6px solid #4caf50; margin-bottom: 20px;  padding: 13px 0; width: 450px; ">Book Details</h1>
+</body>
+</html>
 <?php
 
 $dbcon=parse_ini_file("dbconfig.ini");
@@ -19,39 +29,38 @@ else
 }
 $search = $_REQUEST["search"];
 
-$query = "SELECT isbn,title,author,edition,publication FROM book_detail WHERE title='$search'";
+$res=mysqli_query($conn,"SELECT * FROM book_detail WHERE title='$search';");
 
- 
-if(($conn->query($query))->num_rows>0)
-	 {
-	 	echo"<table>
-<tr>
-<th> ISBN </th>
-<th> Title </th>
-<th> Author </th>
-<th> Edition </th>
-<th> Publication </th>
-</tr>";
-
-while($row = mysqli_fetch_assoc($conn->query($query)))
-{
-echo"<tr>
-<td>".$row["isbn"]."</td>
-<td>".$row["title"]."</td>
-<td>".$row["author"]."</td>
-<td>".$row["edition"]."</td>
-<td>".$row["publication"]."</td>
-</tr>";
-echo"</table>";
-
-	 }
-}
-
+if(mysqli_num_rows($res)==0)
+	{
+		echo " <center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'> No book found. Try again...!</h2></center>";
+	}
 else
 {
-	echo "Error..No books found in the library by the name $search";
-}
- 
+ echo "<table class= 'table table-dark table-hover'>";
+ echo "<tr style='background-color:#28292d;'>";
+ echo "<th>"; echo "Book ID"; echo "</th>";
+ echo "<th>"; echo "Genre"; echo "</th>";
+ echo "<th>"; echo "Title"; echo "</th>";
+ echo "<th>"; echo "Author"; echo "</th>";
+ echo "<th>"; echo "Edition"; echo "</th>";
+ echo "<th>"; echo "Publication"; echo "</th>";
+ echo " </tr>";
+
+ while ($row=mysqli_fetch_assoc($res))
+  {
+ echo "<tr>";
+ echo "<td>"; echo $row['book_id']; echo "</td>";
+ echo "<td>"; echo $row['genre']; echo "</td>";
+ echo "<td>"; echo $row['title']; echo "</td>";
+ echo "<td>"; echo $row['author']; echo "</td>";
+ echo "<td>"; echo $row['edition']; echo "</td>";
+ echo "<td>"; echo $row['publication']; echo "</td>";
+ echo " </tr>";
+ }
+
+ echo " </table>";
+} 
 mysqli_close($conn);
 
 ?> 
