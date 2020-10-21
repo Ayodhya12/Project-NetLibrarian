5e7fea273742025fbe0456f753f85de76499cdc1
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-21 21:31:53 +0530

    Create borrow.php

---
+++ newProject/borrow.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="theme.css">
+</head>
+<body>
+	<div class="container cont1">
+    <div class="login-box">
+		<h1 align="center">Book Details</h1>
+        <h2 style="color:#b1adad;">Borrow a book</h2>
+	    <form method="post" action="">
+            <div class="textbox">
+                <input id="" name="book_id" type="text" placeholder="Enter Book ID Number" required=""> 
+            </div>
+            <div class="textbox">
+	            <input id="" type="text" name="mem_id"  placeholder="Enter Member ID Number" required="">
+            </div>
+            <div class="textbox">
+                <input id="" name="issue_date" type="text" placeholder="Enter Issue Date" required=""> 
+            </div>
+            <div class="textbox">
+	            <input id="" type="text" name="return_date"  placeholder="Enter Return Date" required="">
+            </div> <br><br>
+            <input type="submit" name="Borrow" value="Borrow"  class=" btn"><br><br>
+            <input type="reset" name="Reset"   class=" btn">
+
+    
+        </form>
+    </div>
+  </div>
+</body>
+</html>
