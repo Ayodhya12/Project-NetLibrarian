b25d44dc34cdd90343ed4cf943c8793508d20a05
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-25 14:26:42 +0530

    Update profile.php

--- newProject/profile.php
+++ newProject/profile.php
@@ -1,20 +1,112 @@
- 
- <!DOCTYPE html>
+<!DOCTYPE html>
  <html>
  <head>
  	<title> </title>
  	<link rel="stylesheet" type="text/css" href="theme.css">
- 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
- </head>
- <body>
+ 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+ 	<style>
+ 			.btn1
+		{
+			width: 10%;
+            background: none;
+            border: 2px solid #4caf50;
+            color: #fff;
+            padding-left: 10px;
+            padding-right: 70px;
+            font-size:18px;
+            cursor: pointer;
+		}
+	.sidenav {
+  height: 100%;
+  margin-top: 0px;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #222;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: white;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+.h:hover
+{
+	color:white;
+	width: 300px;
+	height: 50px;
+	background-color: #00544c;
+}
+</style>
+</head>
+<body>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+   <div class="h"> <a href="index.php">Home</a></div>
+  <div class="h"> <a href="book.php">Book Library</a></div>
+  <div class="h"><a href="profile.php">Profile</a></div>
+  <div class="h"><a href="#">Account Details</a></div>
+  <div class="h"><a href="contact_form.php">Contact</a></div>
+  <div class="h"><a href="search.php">Search</a></div>
+</div>
+
+<div id="main">
+  
+  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+</script>
+ 
  	<div class="container">
  		<form action="" method="post">
- 			<button class="btn" style="margin-top: 20px; float: right; width: 70px;" name="submit">Settings</button>
+ 			<button class="btn1" style="margin-top: 20px; float: right; width: 70px;" name="submit">Settings</button>
  		</form>
  		 
            <?php
-                session_start();
-
+                session_start();  
                 $dbcon=parse_ini_file("dbconfig.ini");
                 $server=$dbcon['server'];
                 $user=$dbcon['user'];
@@ -67,11 +159,10 @@
  			}
  				echo "<b>";
  				echo "<table class='table table-dark table-hover'>";
-	 				echo "<tr>";
+	 				echo "<tr>"; 
 	 					echo "<td>";
 	 						echo "<b> First Name: </b>";
 	 					echo "</td>";
-
 	 					echo "<td>";
 	 						echo $row['FirstName'];
 	 					echo "</td>";
