c247b32d30373d3b9882e606a0b8b57445f786c0
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-25 14:26:48 +0530

    Update return.php

--- newProject/return.php
+++ newProject/return.php
@@ -1,30 +1,168 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="theme.css">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="theme.css">
+<style>
+    .sidenav {
+  height: 100%;
+  margin-top: 0px;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #222;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: white;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+.h:hover
+{
+    color:white;
+    width: 300px;
+    height: 50px;
+    background-color: #00544c;
+}
+</style>
 </head>
 <body>
-	<div class="container cont1">
-    <div class="login-box">
-		<h1 align="center">Book Details</h1>
+ 
+<?php
+
+$dbcon=parse_ini_file("dbconfig.ini");
+$server=$dbcon['server'];
+$user=$dbcon['user'];
+$pw=$dbcon['dbpass'];
+$db=$dbcon['dbase'];
+
+
+$conn=mysqli_connect($server,$user,$pw,$db);
+
+if($conn->connect_error)
+{
+    die("Not Connected Properly".$conn->connect_error);
+}
+else
+{
+}
+?>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+            <div style="color: white; margin-left: 60px; font-size: 20px;">
+
+                <?php
+                session_start();
+                if(isset($_SESSION['login_user']))
+
+                {    
+                    echo "Welcome ".$_SESSION['login_user']; 
+                }
+                ?>
+            </div><br><br>
+
+ 
+   <div class="h"> <a href="index.php">Home</a></div>
+  <div class="h"> <a href="book.php">Books</a></div>
+  <div class="h"> <a href="request.php">Book Request</a></div>
+  <div class="h"><a href="information.php">Issue Information</a></div>
+  <div class="h"><a href="return.php">Return Book</a></div>
+</div>
+
+<div id="main">
+  
+  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+</script>
+ <div class="container">
+  <div class="login-box">
+        <h1 align="center">Book Details</h1>
         <h2 style="color:#b1adad;">Return a book</h2>
-	    <form method="post" action="">
+        <form method="post" action="">
             <div class="textbox">
                 <input id="" name="book_id" type="text" placeholder="Enter Book ID Number" required=""> 
             </div>
             <div class="textbox">
-                <input id="" type="mem_id" name="title"  placeholder="Enter Member ID Number" required="">
+                <input id="" type="text" name="Email"  placeholder="Enter Your Email Address" required="">
             </div>
             <div class="textbox">
                 <input id="" name="ret_date" type="text" placeholder="Enter Current Date" required=""> 
             </div><br><br>
-            <input type="submit" name="Return" value="Return"  class=" btn"><br><br>
-            <input type="reset" name="Reset"   class=" btn">
-
-    
+            <input type="submit" name="submit" value="Return"  class=" btn"><br><br>
+            <input type="reset" name="Reset"   class=" btn"> 
         </form>
     </div>
   </div>
 </body>
 </html>
+   <?php
+   if(isset($_POST['submit']))
+        {
+            if(isset($_SESSION['login_user']))
+            {
+               mysqli_query($conn,"INSERT INTO return_books(book_id,Email,currnt_date) VALUES('$_POST[book_id]','$_POST[Email]','$_POST[ret_date]');");
+               echo "<h2 class='alert alert-success' style='width:450px; '>Record Added Successfully</h2>";
+               mysqli_query($conn,"UPDATE  `borrow_books` SET   status='return' WHERE Email='$_POST[Email]' AND book_id='$_POST[book_id]';"); 
+                 
+            }
+            else
+            {
+                 
+                echo "<center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'>Please login first.</h2></center>";
+              
+            }
+        }
+        mysqli_close($conn);
+
+?>  
+</body>
+</html>  
+ 
+ 
+ 
