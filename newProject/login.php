e979c6b458a9baef8834ba0b6d1900b2e7e5cdde
Ayodhya12 <68230139+Ayodhya12@users.noreply.github.com>
2020-10-25 14:26:33 +0530

    Update login.php

--- newProject/login.php
+++ newProject/login.php
@@ -23,22 +23,45 @@ else{
 $phash=sha1($pwd);
 
 
-$sql="SELECT EXISTS(SELECT * FROM `registration` WHERE `Email`='$email' AND `Password`='$phash')";
-
+/*$sql="SELECT EXISTS(SELECT * FROM `registration` WHERE `Email`='$email' AND `Password`='$phash')";*/
+$sql="SELECT * FROM `registration` WHERE `Email`='$email' AND `Password`='$phash'";
 
 $r=mysqli_query($conn,$sql);
   
 
 if($r){
-	 $row=mysqli_fetch_array($r,MYSQLI_NUM);
-	 if ($row[0]==1) {
-	 	session_start();
+	/* $row=mysqli_fetch_array($r,MYSQLI_NUM);
+	 if ($row[0]==1) {*/
+if(mysqli_num_rows($r) > 0)
+{
+	while($row=mysqli_fetch_array($r)){
+		if($row["status_id"]=="1")
+		{
+			session_start();
+		$_SESSION["email"]=$email;
+		$_SESSION["pwd"]=$pwd;
+
+        $_SESSION['login_user']=$email;
+	 	echo "<script type='text/javascript'>location.href= 'admin.php';</script>";
+
+		}
+		else if($row["status_id"]=="2")
+		{
+			session_start();
 		$_SESSION["email"]=$email;
 		$_SESSION["pwd"]=$pwd;
 
         $_SESSION['login_user']=$email;
 	 	echo "<script type='text/javascript'>location.href= 'index.php';</script>";
-	 }
+		}
+	}
+}	 	/*session_start();
+		$_SESSION["email"]=$email;
+		$_SESSION["pwd"]=$pwd;
+
+        $_SESSION['login_user']=$email;
+	 	echo "<script type='text/javascript'>location.href= 'index.php';</script>";
+	 }*/
 	 else{
 	 	echo "<center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'> User not found. Please check your e-mail and password and try again.</h2></center>";
 	 }
