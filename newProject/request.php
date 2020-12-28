422f4650be1b766143d622f516e857342ad1f004
Dilki Ayodhya <68230139+Ayodhya12@users.noreply.github.com>
2020-12-29 00:30:43 +0530

    Update request.php

--- newProject/request.php
+++ newProject/request.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+   <title>NetLibrarian.com</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="theme.css">
+  <link rel="stylesheet" type="text/css" href="theme1.css">
 <style>
   .btn1
   {
    width: 10%;
    background: none;
-   border: 2px solid #4caf50;
+   border: 2px solid #00b289;
    color: #fff;
    padding-left: 10px;
    padding-right: 80px;
@@ -38,7 +38,9 @@
   display: block;
   transition: 0.3s;
 }
-
+img{
+  border-radius: 50%;
+}
 .sidenav a:hover {
   color: white;
 }
@@ -100,8 +102,14 @@ else
                 if(isset($_SESSION['login_user']))
 
                 {    
-                    echo "Welcome ".$_SESSION['login_user']; 
-                }
+                   $q=mysqli_query($conn,"SELECT * FROM registration where Email='$_SESSION[login_user]' ;");
+                   $row=mysqli_fetch_assoc($q);
+                   echo "<div style='align: center'>
+                    <img height=110 width=120 src='upload/".$row['pic']."'>
+                    </div>";  
+                  echo "Welcome "."<br>"./*$_SESSION['login_user']*/$row['FirstName']." ".$row['LastName']; 
+                } 
+                
                 ?>
             </div><br><br>
 
@@ -115,7 +123,7 @@ else
 
 <div id="main">
   
-  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
 
 
 <script>
@@ -135,7 +143,7 @@ function closeNav() {
    <form action="" method="post">
       <button class="btn1" style="margin-top: 10px; float: right; width: 70px;" name="submit">Request</button>
     </form>
-<h1 align="center" style="color: #fff; border-bottom: 6px solid #4caf50; margin-bottom: 20px;  padding: 13px 0; width: 400px; ">List Of Books</h1>
+<h1 align="center" style="color: #fff; border-bottom: 6px solid #00b289; margin-bottom: 20px;  padding: 13px 0; width: 400px; ">List Of Books</h1>
     
    <?php
    if(isset($_POST['submit']))
