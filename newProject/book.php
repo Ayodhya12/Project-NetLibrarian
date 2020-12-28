f9bb73170a62e904dd0b39c666d0b050bb4a5b95
Dilki Ayodhya <68230139+Ayodhya12@users.noreply.github.com>
2020-12-29 00:32:13 +0530

    Update book.php

--- newProject/book.php
+++ newProject/book.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	 <title>NetLibrarian.com</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="theme.css">
+	<link rel="stylesheet" type="text/css" href="theme1.css">
 <style>
 	.sidenav {
   height: 100%;
@@ -18,7 +18,9 @@
   transition: 0.5s;
   padding-top: 60px;
 }
-
+img{
+  border-radius: 50%;
+}
 .sidenav a {
   padding: 8px 8px 8px 32px;
   text-decoration: none;
@@ -56,6 +58,37 @@
 	height: 50px;
 	background-color: #00544c;
 }
+.btn1
+    {
+      width: 10%;
+            background: none;
+            border: 2px solid  #00b289;
+            color: #fff;
+            padding-left: 10px;
+            padding-right: 30px;
+            font-size:18px;
+            cursor: pointer;
+    }
+.textbox1
+    {
+      width: 91%;
+            overflow: hidden;
+            font-size: 20px;
+            padding: 8px 0;
+            margin: 8px 0;
+            border: 2px solid  #00b289;
+    }
+    .textbox1  input
+        {
+           border: none;
+           outline: none;
+           background: none;
+           color: white;
+           font-size: 18px;
+           width: 94%;
+           float: center;
+           margin: 0 10px;
+        }
 </style>
 </head>
 <body>
@@ -89,8 +122,14 @@ else
                 if(isset($_SESSION['login_user']))
 
                 { 	 
-                    echo "Welcome ".$_SESSION['login_user']; 
+                    $q=mysqli_query($conn,"SELECT * FROM registration where Email='$_SESSION[login_user]' ;");
+                    $row=mysqli_fetch_assoc($q);
+                    echo "<div style='align: center'>
+                    <img height=110 width=120 src='upload/".$row['pic']."'>
+                    </div>";  
+                    echo "Welcome "."<br>"./*$_SESSION['login_user']*/$row['FirstName']." ".$row['LastName']; 
                 }
+                
                 ?>
             </div><br><br>
 
@@ -103,7 +142,7 @@ else
 
 <div id="main">
   
-  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
 
 
 <script>
@@ -120,11 +159,27 @@ function closeNav() {
 }
 </script>
  <div class="container">
-<h1 align="center" style="color: #fff; border-bottom: 6px solid #4caf50; margin-bottom: 20px;  padding: 13px 0; width: 400px; ">List Of Books</h1>
+  <div style="padding-left: 850px; ">
+    <form method="post" >
+      <div style="width: 260px;" class="textbox1">
+                <input  type="text" name="search" placeholder="Enter the title of the book">
+            </div>
+        <button  type="submit" name="submit" class="btn1"> <i class="fa fa-search" aria-hidden="true"></i>
+        </button>
+    </form>
+  </div>
+<h1 align="center" style="color: #fff; border-bottom: 6px solid #00b289; margin-bottom: 20px;  padding: 13px 0; width: 400px; ">List Of Books</h1>
 <?php
+   if(isset($_POST['submit']))
+    {
+      $res=mysqli_query($conn,"SELECT * FROM book_detail WHERE title='$_POST[search]';");
 
-$res=mysqli_query($conn,"SELECT * FROM book_detail ORDER BY book_id;");
-
+if(mysqli_num_rows($res)==0)
+  {
+    echo " <center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'> No book found. Try again...!</h2></center>";
+  }
+else
+{
  echo "<table class='table table-dark table-hover'>";
  echo "<tr style='background-color:#28292d;'>";
  echo "<th>"; echo "Book ID"; echo "</th>";
@@ -132,7 +187,8 @@ $res=mysqli_query($conn,"SELECT * FROM book_detail ORDER BY book_id;");
  echo "<th>"; echo "Title"; echo "</th>";
  echo "<th>"; echo "Author"; echo "</th>";
  echo "<th>"; echo "Edition"; echo "</th>";
- echo "<th>"; echo "Publication"; echo "</th>";
+ echo "<th>"; echo "Status"; echo "</th>";
+/* echo "<th>"; echo "Quantity"; echo "</th>";*/
  echo " </tr>";
 
  while ($row=mysqli_fetch_assoc($res))
@@ -143,12 +199,45 @@ $res=mysqli_query($conn,"SELECT * FROM book_detail ORDER BY book_id;");
  echo "<td>"; echo $row['title']; echo "</td>";
  echo "<td>"; echo $row['author']; echo "</td>";
  echo "<td>"; echo $row['edition']; echo "</td>";
- echo "<td>"; echo $row['publication']; echo "</td>";
+ echo "<td>"; echo $row['status']; echo "</td>";
+ /*echo "<td>"; echo $row['quantity']; echo "</td>";*/
  echo " </tr>";
  }
 
  echo " </table>";
+      }
+    }
+    else
+    {
+      $res=mysqli_query($conn,"SELECT * FROM book_detail ORDER BY book_id;");
 
+ echo "<table class='table table-dark table-hover'>";
+ echo "<tr style='background-color:#28292d;'>";
+ echo "<th>"; echo "Book ID"; echo "</th>";
+ echo "<th>"; echo "Genre"; echo "</th>";
+ echo "<th>"; echo "Title"; echo "</th>";
+ echo "<th>"; echo "Author"; echo "</th>";
+ echo "<th>"; echo "Edition"; echo "</th>";
+ echo "<th>"; echo "Status"; echo "</th>";
+ echo "<th>"; echo "Quantity"; echo "</th>";
+ echo " </tr>";
+
+ while ($row=mysqli_fetch_assoc($res))
+  {
+ echo "<tr>";
+ echo "<td>"; echo $row['book_id']; echo "</td>";
+ echo "<td>"; echo $row['genre']; echo "</td>";
+ echo "<td>"; echo $row['title']; echo "</td>";
+ echo "<td>"; echo $row['author']; echo "</td>";
+ echo "<td>"; echo $row['edition']; echo "</td>";
+ echo "<td>"; echo $row['status']; echo "</td>";
+ echo "<td>"; echo $row['quantity']; echo "</td>";
+ echo " </tr>";
+ }
+
+ echo " </table>";
+    }
+  
 mysqli_close($conn);
 
 ?> 
