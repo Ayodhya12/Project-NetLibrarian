0f9a7264eb1104b9cbcfadd193d7de0912c2eaa6
Dilki Ayodhya <68230139+Ayodhya12@users.noreply.github.com>
2020-12-29 00:32:18 +0530

    Update admin_signup.php

--- newProject/admin_signup.php
+++ newProject/admin_signup.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="theme.css">
+<link rel="stylesheet" type="text/css" href="theme1.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <?php
 session_start();
@@ -17,12 +17,24 @@ if($conn->connect_error)
 	die("Not Connected Properly".$conn->connect_error);
 }
 else{
-
+$count=0;
 $firstname=$_REQUEST["fname"];
 $lastname=$_REQUEST["lname"];
 $email=$_REQUEST["mail"];
 $pwd=sha1($_REQUEST["pwd"]);
 
+$r="SELECT Email from registration";
+$res=mysqli_query($conn,$r); 
+
+        while($row=mysqli_fetch_assoc($res))
+        {
+          if($row['Email']==$email)
+          {
+            $count=$count+1;
+          }
+        }
+if($count==0)
+{
 $query="INSERT INTO registration(FirstName,LastName,Email,Password,status_id) VALUES('$firstname','$lastname','$email','$pwd','1');";
 
 if($conn->query($query))
@@ -33,8 +45,14 @@ else
 {
 	echo "<center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'>ERROR</h2></center>";
 }
+}
+else
+{
+	echo "<center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'>The Email Already Exist</h2></center>";
+}
  
 mysqli_close($conn);
 }
  ?>
+<center><h2 class='alert alert-dark' style='width:450px; margin-top:40px;'><a href="index.php" style=" text-decoration: none;"> click here </a></h2></center>
  
