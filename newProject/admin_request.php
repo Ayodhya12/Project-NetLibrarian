280a2528abbc03f615b93c1da0d39d9077111eee
Dilki Ayodhya <68230139+Ayodhya12@users.noreply.github.com>
2020-12-29 00:29:28 +0530

    Update admin_request.php

--- newProject/admin_request.php
+++ newProject/admin_request.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+     <title>NetLibrarian.com</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="theme.css">
+    <link rel="stylesheet" type="text/css" href="theme1.css">
 <style>
     .sidenav {
   height: 100%;
@@ -18,7 +18,9 @@
   transition: 0.5s;
   padding-top: 60px;
 }
-
+img{
+  border-radius: 50%;
+}
 .sidenav a {
   padding: 8px 8px 8px 32px;
   text-decoration: none;
@@ -60,7 +62,7 @@
 {
 	width: 10%;
   background: none;
-  border: 2px solid #4caf50;
+  border: 2px solid  #00b289;
   color: #fff;
   padding-left: 10px;
   padding-right: 70px;
@@ -71,7 +73,7 @@
 </head>
 <body>
 
-<?php
+<?php 
 
 $dbcon=parse_ini_file("dbconfig.ini");
 $server=$dbcon['server'];
@@ -84,24 +86,30 @@ $conn=mysqli_connect($server,$user,$pw,$db);
 
 if($conn->connect_error)
 {
-    die("Not Connected Properly".$conn->connect_error);
+  die("Not Connected Properly".$conn->connect_error);
 }
 else
 {
 }
 ?>
+ 
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-
-            <div style="color: white; margin-left: 60px; font-size: 20px;">
+        <div style="color: white; margin-left: 60px; font-size: 20px;">
 
                 <?php
                 session_start();
                 if(isset($_SESSION['login_user']))
 
                 {    
-                    echo "Welcome ".$_SESSION['login_user']; 
+                    $q=mysqli_query($conn,"SELECT * FROM registration where Email='$_SESSION[login_user]' ;");
+                    $row=mysqli_fetch_assoc($q);
+                    echo "<div style='align: center'>
+                    <img height=110 width=120 src='upload/".$row['pic']."'>
+                    </div>";  
+                    echo "Welcome "."<br>"./*$_SESSION['login_user']*/$row['FirstName']." ".$row['LastName']; 
                 }
+                
                 ?>
             </div><br><br>
 
@@ -109,12 +117,13 @@ else
   <div class="h"> <a href="admin.php">Home</a></div>
   <div class="h"> <a href="admin_books.php">Books</a></div>
   <div class="h"> <a href="admin_request.php">Book Request</a></div>
+  <div class="h"><a href="bookinfo.php">Issue Book</a></div>
   <div class="h"><a href="admin_return.php">Return Book</a></div>
 </div>
 
 <div id="main">
   
-  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+  <span style="color:#fff; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
 
 
 <script>
@@ -136,7 +145,7 @@ function closeNav() {
  			<button class="btn1" style="margin-top: 20px; float: right; width: 170px;" name="submit">Approve_Request</button>
  		</form>
 
-	<h1 align="center" style="color: #fff; border-bottom: 6px solid #4caf50; margin-bottom: 20px;  padding: 13px 0; width: 400px; ">Request of Book</h1>
+	<h1 align="center" style="color: #fff; border-bottom: 6px solid  #00b289; margin-bottom: 20px;  padding: 13px 0; width: 400px; ">Request of Book</h1>
    <?php
 
    if(isset($_POST['submit']))
@@ -150,7 +159,7 @@ function closeNav() {
 
 	if(isset($_SESSION['login_user']))
 	{
-		$sql= "SELECT registration.Email,book_detail.book_id,title,author,edition FROM registration inner join borrow_books ON registration.Email=borrow_books.Email inner join book_detail ON  book_detail.book_id=borrow_books.book_id WHERE borrow_books.approve =''";
+		$sql= "SELECT registration.Email,book_detail.book_id,genre,title,author,edition/*,status,quantity*/ FROM registration inner join borrow_books ON registration.Email=borrow_books.Email inner join book_detail ON  book_detail.book_id=borrow_books.book_id WHERE borrow_books.approve =''";
 		$res= mysqli_query($conn,$sql);
 
 		if(mysqli_num_rows($res)==0)
@@ -161,25 +170,30 @@ function closeNav() {
 			}
 		else
 		{
-			echo "<table class='table table-dark hover' >";
-			echo "<tr style='background-color:#28292d;'>"; 
-				echo "<th>"; echo "Email";  echo "</th>"; 
-				echo "<th>"; echo "Book ID";  echo "</th>";
-				echo "<th>"; echo "Book Name";  echo "</th>";
-				echo "<th>"; echo "Authors Name";  echo "</th>";
-				echo "<th>"; echo "Edition";  echo "</th>"; 
-			echo "</tr>";	
+		echo "<table class='table table-dark table-hover'>";
+    echo "<tr style='background-color:#28292d;'>";
+    echo "<th>"; echo "Student Email"; echo "</th>";
+    echo "<th>"; echo "Book ID"; echo "</th>";
+    echo "<th>"; echo "Genre"; echo "</th>";
+    echo "<th>"; echo "Title"; echo "</th>";
+    echo "<th>"; echo "Author"; echo "</th>";
+    echo "<th>"; echo "Edition"; echo "</th>";
+    /*echo "<th>"; echo "Status"; echo "</th>";
+    echo "<th>"; echo "Quantity"; echo "</th>";*/
+    echo " </tr>";
 
-			while($row=mysqli_fetch_assoc($res))
-			{
-				echo "<tr>";
-				echo "<td>"; echo $row['Email']; echo "</td>";
-				echo "<td>"; echo $row['book_id']; echo "</td>";
-				echo "<td>"; echo $row['title']; echo "</td>";
-				echo "<td>"; echo $row['author']; echo "</td>";
-				echo "<td>"; echo $row['edition']; echo "</td>";
-				
-				echo "</tr>";
+ while ($row=mysqli_fetch_assoc($res))
+  {
+    echo "<tr>";
+    echo "<td>"; echo $row['Email']; echo "</td>";
+    echo "<td>"; echo $row['book_id']; echo "</td>";
+    echo "<td>"; echo $row['genre']; echo "</td>";
+    echo "<td>"; echo $row['title']; echo "</td>";
+    echo "<td>"; echo $row['author']; echo "</td>";
+    echo "<td>"; echo $row['edition']; echo "</td>";
+   /* echo "<td>"; echo $row['status']; echo "</td>";
+    echo "<td>"; echo $row['quantity']; echo "</td>";*/
+    echo " </tr>";
 			}
 		echo "</table>";
 		}
@@ -188,8 +202,8 @@ function closeNav() {
 	{
 		?>
 		<br>
-			<h4 style="text-align: center;color: yellow;">You need to login to see the request.</h4>
-			
+    <center><h2 class='alert alert-dark' style='width:450px; margin-top:250px;'>You need to login to see the request.</h2></center>
+			  
 		<?php
 	}
 
